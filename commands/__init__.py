9a010e4ec99d681e9d907ee87d55c24bff572250
vina-cpu <victorianawalany@gmail.com>
2025-03-16 12:39:20 -0600

    configuring logging and tests correctly to not expose environment variables

--- commands/__init__.py
+++ commands/__init__.py
@@ -6,34 +6,40 @@ from commands.command import CommandHandler
 
 class Interface:
     def __init__(self):
-        self.configure_logging()
         load_dotenv()
-        #logging.write_loc = self.load_env() # saving my environment variable to where to save log files
+        self.logging_level = self.load_env("LOG_LEVEL") # saving my environment variable to where to save log files
+        self.logging_destination = self.load_env("LOG_DESTINATION")
+        self.configure_logging(self.logging_level, self.logging_destination)
         self.commandHandler = CommandHandler()
     
     @staticmethod
     def newInterface():
         return Interface()
     
-    def configure_logging(self):
-        log_dir = "logs"
+    def configure_logging(self, logging_level, logging_destination):
+        log_dir = logging_destination
         os.makedirs(log_dir, exist_ok = True)
         log_file = os.path.join(log_dir, datetime.now().strftime("%m.%d.%H.%M.log"))
+        log_level = getattr(logging, logging_level.upper()) # maps the logging_level to right logging, also makes sure it's upppercase
         logging.basicConfig(
-            level=logging.INFO, 
+            level=log_level,
             format='%(asctime)s %(levelname)s %(message)s',
             datefmt="%m-%d-%Y %H:%M:%S",
             filename=log_file
         )
-        logging.info("Logging configured")
+        logging.info("Logging configured, environment variables loaded!")
     
-    def load_env(self):
-        logging.info("Environment variable loaded")
-        key: str = os.getenv("MYKEY")
+    def load_env(self, reference_key):
+        key: str = os.getenv(reference_key)
         return key
     
-    def get_env(self):
-        return self.myEnvironment
+    def get_env(self, reference_key) -> str:
+        if reference_key == "LOG_LEVEL":
+            return self.logging_level
+        elif reference_key == "LOG_DESTINATION":
+            return self.logging_destination
+        else:
+            return None
 
     def start(self):
         print("Hello! Type calculator commands to utilize the calculator, type 'menu' for a full list of commands including saving and loading, or type 'exit' to exit!")
--- tests/test_log_and_env.py
+++ tests/test_log_and_env.py
@@ -1,7 +1,15 @@
 '''My Tests for Logging and Environment Variables'''
 from pathlib import Path
+from commands import Interface
 
 def test_exists_log_file():
-    '''Test to show at least one file is in logs file'''
+    '''Test to show at least one file is where i put my log files'''
     # adding log files down to the minute, don't want to test by number of files here
-    assert any(Path("logs").iterdir()) is True
+    myinterface = Interface.newInterface()
+    assert any(Path(myinterface.get_env("LOG_DESTINATION")).iterdir()) is True
+
+def test_env_var():
+    '''Test to test that environment variables are inside of Interface'''
+    myinterface = Interface.newInterface()
+    assert myinterface.get_env("LOG_LEVEL") is not None
+    assert myinterface.get_env("LOG_DESTINATION") is not None
