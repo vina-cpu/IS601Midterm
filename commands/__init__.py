f5794bc072a55501bd2163e2eade9ed9e352be79
vina-cpu <victorianawalany@gmail.com>
2025-03-16 05:56:03 -0600

    got kinda messy but did things for two different branches

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,11 +1,15 @@
 import os
 import logging
+from dotenv import load_dotenv
 from datetime import datetime
 from commands.command import CommandHandler
+from history import History
 
 class Interface:
     def __init__(self):
         self.configure_logging()
+        load_dotenv()
+        History.read_csv_loc = self.loadEnv() # saving my environment variable to where to read csvs for this 
         self.commandHandler = CommandHandler()
     
     @staticmethod
@@ -13,16 +17,24 @@ class Interface:
         return Interface()
     
     def configure_logging(self):
-        logDir = "logs"
-        os.makedirs(logDir, exist_ok = True)
-        logFile = os.path.join(logDir, datetime.now().strftime("%m.%d.%H.%M.log"))
+        log_dir = "logs"
+        os.makedirs(log_dir, exist_ok = True)
+        log_file = os.path.join(log_dir, datetime.now().strftime("%m.%d.%H.%M.log"))
         logging.basicConfig(
             level=logging.INFO, 
             format='%(asctime)s %(levelname)s %(message)s',
             datefmt="%m-%d-%Y %H:%M:%S",
-            filename=logFile
+            filename=log_file
         )
         logging.info("Logging configured")
+    
+    def load_env(self):
+        logging.info("Environment variable loaded")
+        key: str = os.getenv("MYKEY")
+        return key
+    
+    def get_env(self):
+        return self.myEnvironment
 
     def start(self):
         print("Hello! Type calculator commands to utilize the calculator, type 'menu' for a full list of commands, or type 'exit' to exit!")
--- history/__init__.py
+++ history/__init__.py
@@ -1,5 +1,7 @@
+import os
 import pandas as pd
 from decimal import Decimal
+from datetime import datetime
 from typing import Callable, List
 from calculator.calculation import Calculation
 
@@ -15,10 +17,16 @@ class History:
         return cls.hist
 
     @classmethod
-    def load_history(cls, hist):
-    
+    def load_history(cls, hist, path: str): # going to be in same file as environment variable
+        new_data = pd.read_csv(path)
+        hist = pd.concat([hist, new_data], ignore_index = True)
+
     @classmethod
     def save_history(cls, hist):
+        save_dir = "logs"
+        os.makedirs(save_dir, exist_ok = True)
+        save_file = os.path.join(save_dir, datetime.now().strftime("%m.%d.%H.%M.log"))
+        
         
     @classmethod
     def clear_history(cls):
